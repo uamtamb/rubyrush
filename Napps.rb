bec76ae367301700ba59a82233cd6ffe112bf96f
Андрей <53381640+uamtamb@users.noreply.github.com>
2019-08-26 01:43:34 +0300

    Update Napps.rb

--- Napps.rb
+++ Napps.rb
@@ -1,15 +1,15 @@
 # Метод - меню выбора программ
 def Napp (apps)
-    puts "Программа № #{apps}"
-    while (apps==nil || apps==" ")
+    puts "\nПрограмма № #{apps}"
+    if (apps==nil || apps==" " || apps!=0 || apps!=1 || apps!=2 || apps!=3 || apps!=4)
+        puts "НЕ ВЕРНО! Введите значение 1,2,3,4 или 0"
         puts "0. Выход из программы"
         puts "1. Вывести большее из чисел"
         puts "2. Среднее арифметическое двух чисел"
         puts "3. Четное ли число?"
         puts "4. Сегодня выходной?"
-        puts "Введите значение 1,2,3,4 или 0"
         print "Введите номер программы: "
-        apps=STDIN.gets.to_i
+        apps=STDIN.gets
     end
     return apps
 end
